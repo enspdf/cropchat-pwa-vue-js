b8b9c82ee5790cac7150124455abd83bdfe6cd89
sebastian Higuita castañeda <sebastianhiguitaca@gmail.com>
2017-11-27 04:36:34 -0500

    added firebase && vuefire

---
+++ src/service/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+var config = {
+  apiKey: "AIzaSyBm_xghmotbuYs_wPrcbMDqM-ucM_Qex3M",
+  authDomain: "cropchat-9779e.firebaseapp.com",
+  databaseURL: "https://cropchat-9779e.firebaseio.com",
+  projectId: "cropchat-9779e",
+  storageBucket: "cropchat-9779e.appspot.com",
+  messagingSenderId: "427472067361"
+}
+
+firebase.initializeApp(config)
+
+export default {
+  database: firebase.database()
+}
