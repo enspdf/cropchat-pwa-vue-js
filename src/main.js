b5b81ac0cca226ef659f70d57ef410dcc812eb37
sebastian Higuita castañeda <sebastianhiguitaca@gmail.com>
2017-11-27 05:19:47 -0500

    added resources

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuefire from 'vuefire'
+import firebase from './service/firebase'
+import VueResource from 'vue-resource'
 
 Vue.use(Vuefire)
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 var config = {
-  apiKey: "AIzaSyBm_xghmotbuYs_wPrcbMDqM-ucM_Qex3M",
-  authDomain: "cropchat-9779e.firebaseapp.com",
-  databaseURL: "https://cropchat-9779e.firebaseio.com",
-  projectId: "cropchat-9779e",
-  storageBucket: "cropchat-9779e.appspot.com",
-  messagingSenderId: "427472067361"
+  apiKey: 'AIzaSyBm_xghmotbuYs_wPrcbMDqM-ucM_Qex3M',
+  authDomain: 'cropchat-9779e.firebaseapp.com',
+  databaseURL: 'https://cropchat-9779e.firebaseio.com',
+  projectId: 'cropchat-9779e',
+  storageBucket: 'cropchat-9779e.appspot.com',
+  messagingSenderId: '427472067361'
 }
 
 firebase.initializeApp(config)
